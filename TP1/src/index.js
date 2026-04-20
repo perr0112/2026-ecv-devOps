266bd77a91e36012b2d7a52c68e0d0c7b8628bfe
perr0112 <prtclement.ctc@gmail.com>
2026-04-20 13:38:12 +0200

    feat: add connexion to MongoDB + retrieve all teams from database instead of json

--- TP1/src/index.js
+++ TP1/src/index.js
@@ -1,9 +1,24 @@
 import express from 'express'
+import { MongoClient } from 'mongodb'
 
 import equipes from "../data/equipes.json" with { type: "json" }
 
 const app = express()
 const PORT = 82
+const MONGO_URI = process.env.MONGO_URI || 'mongodb://127.0.0.1:27017'
+const MONGO_DB_NAME = process.env.MONGO_DB_NAME || 'tp1'
+
+let mongoClient
+let db
+
+async function connectMongo() {
+    mongoClient = new MongoClient(MONGO_URI)
+    await mongoClient.connect()
+    db = mongoClient.db(MONGO_DB_NAME)
+    await db.command({ ping: 1 })
+
+    console.log(`MongoDB connecté (${MONGO_DB_NAME})`)
+}
 
 /**
  * Middleware
@@ -18,9 +33,17 @@ app.use(express.json())
 /**
  * Routes - {GET}
  */
-app.get('/equipes', (req, res) => {
+app.get('/equipes', async (req, res) => {
     // res.send('Liste des équipes')
-    res.send(equipes)
+    // res.send(equipes)
+
+    try {
+        const docs = await db.collection('equipes').find({}).toArray()
+        res.status(200).json(docs)
+    } catch (err) {
+        console.error('Erreur MongoDB :', err)
+        res.status(500).json({ error: err.message })
+    }
 })
 
 app.get('/equipes/:id', (req, res) => {
@@ -68,7 +91,20 @@ app.delete('/equipes/:id', (req, res) => {
 
 
 // Listener
-app.listen(PORT, () => {
-    console.log(`Application lancée sur le port ${PORT}`)
-    console.log(`http://127.0.0.1:${PORT}`)
-})
+async function startServer() {
+    console.log("[startServer] Tentative de connexion en cours")
+    try {
+        console.log("[try] Tentative de connexion en cours")
+        await connectMongo()
+
+        app.listen(PORT, () => {
+            console.log(`Application lancée sur le port ${PORT}`)
+            console.log(`http://127.0.0.1:${PORT}`)
+        })
+    } catch (error) {
+        console.error('Impossible de se connecter à MongoDB :', error.message)
+        process.exit(1)
+    }
+}
+
+startServer()
